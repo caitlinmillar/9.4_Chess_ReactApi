0051e54082aeaed1a14cdd76eab589f0f5325200
Caitlin Millar <caitlin-millar@outlook.com>
2023-01-26 20:27:07 +0000

    added a react router

--- src/containers/ChessContainer.js
+++ src/containers/ChessContainer.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Player from "../components/Player";
 import PlayerList from "../components/PlayerList";
 import PlayerForm from "../components/PlayerForm";
@@ -35,17 +36,32 @@ const ChessContainer = () => {
 
 
     return(
+        <BrowserRouter>
         <div className="container">
             <div className="header_bar">
                 <h2 className="header">Chess.com </h2>
                 <div className="form">
                     <PlayerForm players={players}/>
                 </div>
-            </div>    
-                <div className="GM_List">
-                    <PlayerList players={players}/>
-                </div>
-        </div>    
+            </div>   
+            <ul>
+                
+                <li>
+                    <Link to="/GrandMasters">List of GrandMasters</Link>
+                </li>
+            </ul> 
+            <div className="GM_List">
+            <Routes>
+                
+                    <Route path="/GrandMasters" element={
+                    <PlayerList 
+                        players={players}/>}
+                    />
+             
+            </Routes>    
+            </div>
+        </div> 
+        </BrowserRouter>
     )
 }
 
